83437b9f05dbb351475c16f7923366cc33ff97f1
NadiaBdh <nadia.boudhief@esprit.tn>
2019-07-10 02:04:27 +0100

    refactoring

--- spring-boot-data-jpa-mvc-jsp/src/main/java/tn/esprit/spring/entity/University.java
+++ spring-boot-data-jpa-mvc-jsp/src/main/java/tn/esprit/spring/entity/University.java
@@ -12,65 +12,72 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
 @Entity
-public class University implements Serializable{
-	
-	
+public class University implements Serializable {
+
 	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	private Long id; 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String location;
 	private String title;
 	private String description;
 	private Float score;
 	@Temporal(TemporalType.DATE)
 	private Date deadline;
-	
+
 	@ManyToOne
 	private User user;
-	
+
 	@ManyToMany
 	private List<Major> majors;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getLocation() {
 		return location;
 	}
+
 	public void setLocation(String location) {
 		this.location = location;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public Float getScore() {
 		return score;
 	}
+
 	public void setScore(Float score) {
 		this.score = score;
 	}
+
 	public Date getDeadline() {
 		return deadline;
 	}
+
 	public void setDeadline(Date deadline) {
 		this.deadline = deadline;
 	}
-	
-	
-	
-
-
 
 }
